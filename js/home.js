8828492f8592609887965912dc96740510965f2c
fleopardo <fleopardo@gmail.com>
2015-03-29 11:59:12 -0300

    home

--- js/home.js
+++ js/home.js
@@ -8,6 +8,28 @@
 		});
 	}
 
+	if( $(".owl-carousel-detalle").length > 0 ){
+		$('.owl-carousel-detalle').owlCarousel({
+		    loop:false,
+		    margin:0,
+		    nav:true,
+		    dots:false,
+		    responsiveClass:true,
+		    navText:['<img src="css/assets/owl-prev.png" alt="">','<img src="css/assets/owl-next.png" alt="">'],
+		    responsive:{
+		        0:{
+		            items:1
+		        },
+		        768:{
+		            items:1
+		        },
+		        1024:{
+		            items:1
+		        }
+		    }
+		});
+	}
+
 })();
 
 
